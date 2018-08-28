1825ad422aafe73b256e2a4961ad3640b613daa2
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-28 20:58:50 +0100

    final commit.

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const e621 = require('./e621.js');
 
-var request = e621.random("m/m", "E", 1, body => {
-
-console.log(body);
-
+var request = e621.random("m/m", "E", 1, post => {
+console.log('tags: ' + post.tags);
+console.log('File URL: ' + post.file_url);
+console.log('artists: ' + post.artist);
 });
