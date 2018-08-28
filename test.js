7d97469415315c55efe489358fc71919ebabdd22
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-28 21:19:25 +0100

    kjd

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const e621 = require('./e621.js');
 var request = e621.random("m/m", "E", 1, body => {
   var post = JSON.parse(body);
   console.log(post);
-  console.log('tags: ' + post.tags);
-  console.log('File URL: ' + post.file_url);
-  console.log('artists: ' + post.artist);
+  console.log('tags: ' + post['tags']);
+  console.log('File URL: ' + post['file_url']);
+  console.log('artists: ' + post['artist']);
 });
