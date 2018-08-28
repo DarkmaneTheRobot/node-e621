32eb8f8bcb99300c5c734455e438107c3803dc2d
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-28 21:11:20 +0100

    JSON parsing fail.

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const e621 = require('./e621.js');
 
 var request = e621.random("m/m", "E", 1, body => {
-  console.log(body);
   var post = JSON.parse(body);
+  console.log(post);
   console.log('tags: ' + post.tags);
   console.log('File URL: ' + post.file_url);
   console.log('artists: ' + post.artist);
