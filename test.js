062fd462792ffbbc589409065fb07b85389aca6d
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-28 21:00:41 +0100

    :eyes:

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const e621 = require('./e621.js');
 
 var request = e621.random("m/m", "E", 1, body => {
+  console.log(body);
   var post = JSON.parse(body);
   console.log('tags: ' + post.tags);
   console.log('File URL: ' + post.file_url);
