14a5a19e0af154ae8ab7e97b5aa5508ac0869723
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-28 20:02:45 +0100

    :thinking:

--- e621.js
+++ e621.js
@@ -5,26 +5,18 @@ Rating: E
 Agent: Dantè (by Darkmane on e621)
 */
 
-var http = require("https");
+  var request = require("request");
 
-var unirest = require("unirest");
+  var options = { method: 'GET',
+    url: 'https://e621.net/post/index.json',
+    qs: { tags: 'm/m%20order:random+rating:e', limit: '1' },
+    headers:
+     { 'Postman-Token': 'baf9948b-85fb-4a39-9c74-2f71afa67b7e',
+       'Cache-Control': 'no-cache',
+       'User-Agent': 'DarkBartneder/1.0 (by Darkmane on e621)' } };
 
-var req = unirest("GET", "https://e621.net/post/index.json");
+  request(options, { json: true }, function (error, response, body) {
+    if (error) throw new Error(error);
 
-req.query({
-  "tags": "m/m%20order:random+rating:e",
-  "limit": "1"
-});
-
-req.headers({
-  "Postman-Token": "4b545310-f96f-49c3-9bc5-2d6b155ab842",
-  "Cache-Control": "no-cache",
-  "User-Agent": "DarkBartneder/1.0 (by Darkmane on e621)"
-});
-
-
-req.end(function (res) {
-  if (res.error) throw new Error(res.error);
-
-  console.log(res.body);
-});
+    console.log(body.file_url);
+  });
